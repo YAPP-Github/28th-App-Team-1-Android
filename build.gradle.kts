1dee5f5e953de272493a33905d2b7f4ba2e4567a
Shawn Kang <csm06063@naver.com>
2026-06-25 23:45:43 +0900

    chore: add related files and scripts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,33 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
 
-
     // Hilt
     alias(libs.plugins.kotlin.ksp) apply false
     alias(libs.plugins.dagger.hilt.android) apply false
+}
+
+// Apply Spotless formatting on every commit
+tasks.register("installGitHooks") {
+    // Task description
+    group = "git"
+    description = "Configures Git to use hooks from the script directory."
+
+    // Apply
+    doLast {
+        // Find file
+        val hookFile = layout.projectDirectory.file("script/pre-commit").asFile
+        require(hookFile.exists()) {
+            "Missing Git hook: ${hookFile.path}"
+        }
+
+        // Grant execution permission and configure git hooks path
+        hookFile.setExecutable(true)
+        providers
+            .exec {
+                // Change default Git hook directory to './script'
+                commandLine("git", "config", "core.hooksPath", "script")
+            }.result
+            .get()
+            .assertNormalExitValue()
+    }
 }
