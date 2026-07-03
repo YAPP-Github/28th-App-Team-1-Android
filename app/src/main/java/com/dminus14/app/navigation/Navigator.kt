b791749175a427fbde871f340b08c6a7ecc52204
lyh5427 <lyh5426@naver.com>
2026-07-04 02:07:46 +0900

    feat : Stack관리 Navigator 생성

---
+++ app/src/main/java/com/dminus14/app/navigation/Navigator.kt
@@ -0,0 +1,18 @@
+package com.dminus14.app.navigation
+
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.snapshots.SnapshotStateList
+
+class Navigator(
+    startDestination: Any,
+) {
+    val backStack: SnapshotStateList<Any> = mutableStateListOf(startDestination)
+
+    fun goTo(destination: Any) {
+        backStack.add(destination)
+    }
+
+    fun goBack() {
+        backStack.removeLastOrNull()
+    }
+}
