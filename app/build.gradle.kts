4740ca32cc00cc68aa857f7aebdcf03587efca58
lyh5427 <lyh5426@naver.com>
2026-07-04 00:01:24 +0900

    chore : app모듈 main api, impl 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,3 +2,8 @@ plugins {
     alias(libs.plugins.dminus14.android.application)
     alias(libs.plugins.dminus14.android.lint)
 }
+
+dependencies {
+    implementation(project(":feature:main:api"))
+    implementation(project(":feature:main:impl"))
+}
