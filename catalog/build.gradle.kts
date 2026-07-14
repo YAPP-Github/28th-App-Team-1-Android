68c308db8024e585a26a7d6da694b18dd95554df
Shawn Kang <csm06063@naver.com>
2026-07-14 23:24:25 +0900

    chore: 외부 모듈이 카탈로그 리소스에 접근 못 하게 차단

--- catalog/build.gradle.kts
+++ catalog/build.gradle.kts
@@ -1,3 +1,14 @@
+import org.jetbrains.compose.resources.ResourcesExtension
+
 plugins {
     alias(libs.plugins.dminus14.compose.multiplatform.wasm)
+}
+
+// Prevent other modules to access on Catalog's resources
+compose {
+    resources {
+        publicResClass = false
+        packageOfResClass = "com.dminus14.catalog.generated.resources"
+        generateResClass = ResourcesExtension.ResourceClassGeneration.Always
+    }
 }
