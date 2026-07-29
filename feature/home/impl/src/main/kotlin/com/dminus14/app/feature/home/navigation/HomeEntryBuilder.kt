b578309829babf511ab8689a852f08d879f5e7a6
lyh5427 <lyh5426@naver.com>
2026-07-30 03:48:13 +0900

    feat : home impl module 추가

---
+++ feature/home/impl/src/main/kotlin/com/dminus14/app/feature/home/navigation/HomeEntryBuilder.kt
@@ -0,0 +1,11 @@
+package com.dminus14.app.feature.home.navigation
+
+import androidx.navigation3.runtime.EntryProviderScope
+import com.dminus14.app.feature.home.HomeScreen
+import com.dminus14.app.feature.home.api.Home
+
+fun EntryProviderScope<Any>.homeEntryBuilder() {
+    entry<Home> {
+        HomeScreen()
+    }
+}
