9da743735e37c1c41235a90c00946c21ee003d22
lyh5427 <lyh5426@naver.com>
2026-08-04 02:02:41 +0900

    fix : Route naming 수정

--- feature/onboarding/impl/src/main/kotlin/com/dminus14/app/feature/onboarding/navigation/OnBoardingInterviewEntryBuilder.kt
+++ feature/onboarding/impl/src/main/kotlin/com/dminus14/app/feature/onboarding/navigation/OnBoardingInterviewEntryBuilder.kt
@@ -2,13 +2,13 @@ package com.dminus14.app.feature.onboarding.navigation
 
 import androidx.navigation3.runtime.EntryProviderScope
 import com.dminus14.app.feature.onboarding.OnBoardingInterviewScreen
-import com.dminus14.app.feature.onboarding.api.OnBoardingRoute
+import com.dminus14.app.feature.onboarding.api.OnBoardingInterview
 
 fun EntryProviderScope<Any>.onBoardingInterviewEntryBuilder(
     onNavigate: (Any) -> Unit,
     onClose: () -> Unit,
 ) {
-    entry<OnBoardingRoute> {
+    entry<OnBoardingInterview> {
         OnBoardingInterviewScreen(
             onNavigate = onNavigate,
             onClose = onClose,
