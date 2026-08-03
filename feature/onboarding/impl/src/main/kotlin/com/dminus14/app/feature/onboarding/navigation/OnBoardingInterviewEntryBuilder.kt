76cd6dceec87e7ea064b70e25d5dd6ee0cb3345c
lyh5427 <lyh5426@naver.com>
2026-08-04 01:46:16 +0900

    fix : [Comment[ 파일명 수정

--- feature/onboarding/impl/src/main/kotlin/com/dminus14/app/feature/onboarding/navigation/OnBoardingInterviewEntryBuilder.kt
+++ feature/onboarding/impl/src/main/kotlin/com/dminus14/app/feature/onboarding/navigation/OnBoardingInterviewEntryBuilder.kt
@@ -2,13 +2,13 @@ package com.dminus14.app.feature.onboarding.navigation
 
 import androidx.navigation3.runtime.EntryProviderScope
 import com.dminus14.app.feature.onboarding.OnBoardingInterviewScreen
-import com.dminus14.app.feature.onboarding.api.OnBoardingInterview
+import com.dminus14.app.feature.onboarding.api.OnBoardingRoute
 
 fun EntryProviderScope<Any>.onBoardingInterviewEntryBuilder(
     onNavigate: (Any) -> Unit,
     onClose: () -> Unit,
 ) {
-    entry<OnBoardingInterview> {
+    entry<OnBoardingRoute> {
         OnBoardingInterviewScreen(
             onNavigate = onNavigate,
             onClose = onClose,
