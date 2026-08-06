08404204a8f4e286f253aefe82b19b3a537df89c
Shawn Kang <csm06063@naver.com>
2026-08-06 23:56:36 +0900

    feat: 면접 오류 페이지 구현

---
+++ feature/interview/impl/src/main/kotlin/com/dminus14/app/feature/interview/error/InterviewErrorContract.kt
@@ -0,0 +1,25 @@
+package com.dminus14.app.feature.interview.error
+
+import com.dminus14.app.core.common.mvi.MviEffect
+import com.dminus14.app.core.common.mvi.MviIntent
+import com.dminus14.app.core.common.mvi.MviState
+import com.dminus14.app.feature.interview.api.InterviewErrorType
+
+/** 면접 오류 MVI Intent */
+sealed interface InterviewErrorIntent : MviIntent {
+    data object ClickAbort : InterviewErrorIntent
+
+    data object ClickResume : InterviewErrorIntent
+}
+
+/** 면접 오류 MVI State */
+data class InterviewErrorState(
+    val errorType: InterviewErrorType = InterviewErrorType.MIC,
+) : MviState
+
+/** 면접 오류 MVI Effect */
+sealed interface InterviewErrorEffect : MviEffect {
+    data object NavigateToHome : InterviewErrorEffect
+
+    data object ResumeInterview : InterviewErrorEffect
+}
