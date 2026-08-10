16046da86a0474143b7e4cd783f5a5522219875a
lyh5427 <lyh5426@naver.com>
2026-08-11 03:10:29 +0900

    fix : HilitTextTopbar 넓이 제약 minWidth로 수정

--- designsystem/src/commonMain/kotlin/com/dminus14/designsystem/component/topbar/HilitTopBar.kt
+++ designsystem/src/commonMain/kotlin/com/dminus14/designsystem/component/topbar/HilitTopBar.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -73,7 +74,7 @@ fun HilitTopBar(
             }
 
             Column(
-                modifier = Modifier.width(40.dp),
+                modifier = Modifier.sizeIn(40.dp),
                 horizontalAlignment = Alignment.End,
             ) {
                 trailing?.invoke()
